912e9ee02f1dbea78b0c3b8e35bf012af0cc0037
Mr-jiangzhiguo <jiangzhiguo2010@qq.com>
2020-06-15 14:34:34 +0800

    chore: type add OmitType

--- src/_utils/type.ts
+++ src/_utils/type.ts
@@ -5,3 +5,8 @@ export const tupleNum = <T extends number[]>(...args: T) => args;
 export type TPickOption<T, V> = {
   [K in keyof T]: T[K] extends V ? K : never;
 }[keyof T];
+
+export type OmitType<T, U> = Pick<
+  T,
+  { [K in keyof T]: T[K] extends U ? never : K }[keyof T]
+>;
